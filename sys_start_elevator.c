1d8bced840d5d933c2775951d0ab5abfb05ed88d
camirios <cr15h@my.fsu.edu>
2019-03-01 16:44:19 -0500

    Create sys_start_elevator.c

---
+++ sys_start_elevator.c
@@ -0,0 +1,12 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int (*STUB_start_elevator)(void) = NULL;
+EXPORT_SYMBOL(STUB_start_elevator);
+asmlinkage int sys_start_elevator(void) {
+	if (STUB_start_elevator)
+		return STUB_start_elevator();
+	else
+		return -ENOSYS;
+}
