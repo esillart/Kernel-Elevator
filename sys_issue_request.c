ecc609d6b6695ccc38ecd4f0afbac529901e4b5b
camirios <cr15h@my.fsu.edu>
2019-03-01 16:43:22 -0500

    Update sys_issue_request.c

--- sys_issue_request.c
+++ sys_issue_request.c
@@ -2,9 +2,9 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-long (*STUB_issue_request)(int,int,int) = NULL;
+int (*STUB_issue_request)(int,int,int) = NULL;
 EXPORT_SYMBOL(STUB_issue_request);
-asmlinkage long sys_issue_request(int passenger_type, int start_floor, int destination_floor) {
+asmlinkage int sys_issue_request(int passenger_type, int start_floor, int destination_floor) {
 	if (STUB_issue_request)
 		return STUB_issue_request(passenger_type, start_floor, destination_floor);
 	else
